703e4015ee26c553df574a133430cead3ec1e337
Matheus de Pra Andrade <matheus.andrade@marcopolo.com.br>
2023-03-22 21:36:56 -0300

    smooth function and alpha conversion

--- kiskadee/thermogravimetricAnalysis.py
+++ kiskadee/thermogravimetricAnalysis.py
@@ -2,7 +2,7 @@ from dataLoader import DataLoader
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter
+from scipy.signal import savgol_filter, filtfilt
 
 def massLoss(mass):
     result = []
@@ -37,12 +37,14 @@ def dtg( mass_loss, temperature ):
     return np.gradient(mass_loss) / np.gradient(temperature)
 
 #Retira ruidos
-def smooth(p_Data):
-    
-    window = 21
-    order = 2
-    l_Result = savgol_filter(p_Data, window, order)
-    return l_Result
+def savgolSmooth(data, window:  int, order: int):
+
+    return savgol_filter(data, window, order)
+
+
+filterSwitch = {
+    'savgol' : savgolSmooth
+}
 
 
 class TGA(DataLoader):
@@ -103,9 +105,73 @@ class TGA(DataLoader):
 
             self.data[sample][k]['dtg'] = dtg(_mass_loss, _temperature)
 
-        
+    def dtgFull(self):
+        for sample in self.sampleNames:
+            self.dtg(sample)
+
     # TODO => implement an ALL alterative
-    def plotDTG(self, sample: str):
+    def plotDTG(self, sample: str, smoothed: bool = False):
+
+        # Check if provided sample name is valid!
+        if sample not in self.sampleNames:
+            raise ValueError(f'{sample} sample not found! Please use a valid sample name')
+
+        columnName = 'dtg_smoothed' if smoothed else 'dtg'
+
+        # Get sample
+        data = self.data.get(sample, None)
+
+        if data is None: raise ValueError(f'{sample} sample not found! Please use a valid sample name')
+
+        for k, v in data.items():
+            plt.plot( v["temperature"], v[columnName], label=f'{k} °C/min')
+
+        # Plot labels and legends
+        plt.xlabel("Temperature (°C)")
+        plt.ylabel("DTG")
+        plt.legend( loc="upper right", bbox_to_anchor=(0.98, 0.98), shadow=False, fontsize='large')
+        plt.title( f'DTG - {sample}' )
+
+    def plotDTGFull(self, smoothed: bool = False):
+        for sample in self.sampleNames:
+            self.plotDTG(sample, smoothed)
+
+    def removeData(self, index_to_drop, sample:str, heating_rate: int or float):
+
+        self.data[sample][heating_rate].drop(index=index_to_drop, axis=0, inplace=True)
+        self.data[sample][heating_rate].reset_index(inplace=True)
+
+        
+    def smoothData(
+            self, sample: str, 
+            heating_rate: int or float, 
+            window: int = 20,
+            order: int = 2,
+            column: str = 'dtg',
+            filter: str = 'savgol'
+            ):
+       
+        filterFunc = filterSwitch[filter]
+
+        newColumnName = f'{column}_smoothed'
+        data = self.data[sample][heating_rate][column]
+
+        if filter == 'savgol':
+            self.data[sample][heating_rate][newColumnName] = filterFunc(data, window, order)
+        else:
+            raise ValueError('Select a valid filter!')
+        
+    def smootDataFull(self,            
+            window: int = 20,
+            order: int = 2,
+            column: str = 'dtg',
+            filter: str = 'savgol'):
+        
+        for sample in self.sampleNames:
+            for hr in self.sampleHeatingRates[sample]:
+                self.smoothData(sample, hr, window, order, column, filter)
+   
+    def plotAlpha(self, sample: str):
 
         # Check if provided sample name is valid!
         if sample not in self.sampleNames:
@@ -117,16 +183,10 @@ class TGA(DataLoader):
         if data is None: raise ValueError(f'{sample} sample not found! Please use a valid sample name')
 
         for k, v in data.items():
-            plt.plot( v["temperature"], v["dtg"], label=f'{k} °C/min')
+            plt.plot( v["temperature"], v["alpha"], label=f'{k} °C/min')
 
         # Plot labels and legends
         plt.xlabel("Temperature (°C)")
-        plt.ylabel("DTG")
-        plt.legend( loc="upper right", bbox_to_anchor=(0.98, 0.98), shadow=False, fontsize='large')
-        plt.title( f'DTG - {sample}' )
-
-    def removeData(self, index_to_drop, sample:str, heating_rate: int or float):
-
-        self.data[sample][heating_rate].drop(index=index_to_drop, axis=0, inplace=True)
-        self.data[sample][heating_rate].reset_index(inplace=True)
-        
+        plt.ylabel("Conversion (α)")
+        plt.legend( loc="upper right", bbox_to_anchor=(0.98, 0.40), shadow=False, fontsize='large')
+        plt.title( f'Alpha Conversion - {sample}' )
