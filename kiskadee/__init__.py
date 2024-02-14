9a67bc8dcc0ef2495c6574f9abbfcbb4a1f7c7f1
matheusdepra <matheus-1850@hotmail.com>
2024-02-13 22:19:18 -0300

    fixed init.py

--- kiskadee/__init__.py
+++ kiskadee/__init__.py
@@ -5,8 +5,8 @@ Kiskadee Python Development Tools
 .. currentmodule:: kiskadee
 '''
 
-import dataLoader, kinetics, thermogravimetricAnalysis
-from . import future
+from . import dataLoader, kinetics, thermogravimetricAnalysis
+
 # __all__ = [s for s in dir() if not s.startswith('_')]
 __all__ = ['dataLoader', 
            'kinetics',
